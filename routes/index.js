34a5f77b6e0ead4288877f1fa18b5001d526a5b1
emadsarwani <esarwani341@gmail.com>
2021-04-17 18:35:49 -0400

    updated db

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('index') //what files are we rendering 
+  res.render('index')
 })
 
 module.exports = router
